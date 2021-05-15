5c22e0b2a2333dd6b2cd0523d19e77fef324d301
mena-hub <mssz.nnia@gmail.com>
2021-05-15 16:09:38 -0300

    URLconf module (bigbox/urls.py)

--- challenge/urls.py
+++ challenge/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from bigbox import views
+from bigbox.urls import bigbox_patterns
 
 urlpatterns = [
+    path('', views.home, name="home"),
+    path('box/', include(bigbox_patterns)),
     path('admin/', admin.site.urls),
 ]
